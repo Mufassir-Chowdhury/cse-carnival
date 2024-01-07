9560f7d3f6d491e51af5bffb7deba87870e394df
Mufassir-Chowdhury <mac22214u@gmail.com>
2024-01-07 19:23:44 +0600

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,46 +7,46 @@ import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-sv
 
 const Footer = () => {
     return (
-        <footer className="bg-footer-texture bg-cover px-4 z-50 relative">
-            <div className="flex justify-center items-center space-x-5 md:space-x-10 lg:space-x-32">
+        <footer className="bg-footer-texture bg-cover px-4 py-4 z-50 relative">
+            <div className="flex justify-around items-center space-x-5 md:space-x-10 lg:space-x-32">
                 {/* First Section: Logo */}
-                <div className="lg:mr-8 hidden md:block">
+                <div className="lg:mr-8 ">
                     <img src="/image/logo/Carnival logo.png" alt="Footer Logo" className="h-32 lg:h-40" />
                 </div>
 
                 {/* Second Section: Links */}
-                <div className="text-navbar-link text-sm">
-                    <div className="">
-                        <img src="/image/logo/Carnival logo.png" alt="Footer Logo" className="h-20 w-32 md:hidden" />
-                    </div>
-                    <p className="text-lg font-bold mb-4 hidden md:block">Social Media Links</p>
-                    <div className="flex space-x-4">
-                        {/* Facebook */}
-                        <a href="https://www.facebook.com/your-facebook-page" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faFacebook} className="text-2xl hover:text-blue-500" />
-                        </a>
-                        
-                        {/* Instagram */}
-                        <a href="https://www.instagram.com/your-instagram-account" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faInstagram} className="text-2xl hover:text-pink-500" />
-                        </a>
-                        
-                        {/* LinkedIn */}
-                        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faLinkedin} className="text-2xl hover:text-blue-500" />
-                        </a>
-                    </div>
-                </div>
+                <div className='flex flex-col sm:flex-row justify-between gap-6 sm:gap-20 md:gap-32'>
 
-                {/* Third Section: Title, Description, Email Textbox, and Send Button */}
-                <div className="text-navbar-link">
-                    <h4 className="">
+                    <div className="text-navbar-link text-sm">
+                        <p className="text-lg font-bold mb-4 hidden sm:block">Social Media Links</p>
+                        <div className="flex space-x-4">
+                            {/* Facebook */}
+                            <a href="https://www.facebook.com/your-facebook-page" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faFacebook} className="text-2xl hover:text-blue-500" />
+                            </a>
+                            
+                            {/* Instagram */}
+                            <a href="https://www.instagram.com/your-instagram-account" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faInstagram} className="text-2xl hover:text-pink-500" />
+                            </a>
+                            
+                            {/* LinkedIn */}
+                            <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faLinkedin} className="text-2xl hover:text-blue-500" />
+                            </a>
+                        </div>
+                    </div>
 
-                        &copy; {new Date().getFullYear()} CSE Society, SUST
-                        <p>
-                            All rights reserved.
-                        </p>
-                    </h4>
+                    {/* Third Section: Title, Description, Email Textbox, and Send Button */}
+                    <div className="text-navbar-link">
+                        <h4 className="">
+
+                            &copy; {new Date().getFullYear()} CSE Society, SUST
+                            <p>
+                                All rights reserved.
+                            </p>
+                        </h4>
+                    </div>
                 </div>
             </div>
         </footer>
