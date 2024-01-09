363c56125eb00cab5d83e4348a1a527b5950422d
Mufassir-Chowdhury <mac22214u@gmail.com>
2024-01-09 22:01:58 +0600

    fixed width for announcement and rulebook tab

--- src/pages/Registration/DLSprintAnnouncement.jsx
+++ src/pages/Registration/DLSprintAnnouncement.jsx
@@ -2,7 +2,7 @@ import { PrimaryButton } from "../../components/Button";
 
 const DLSprintRegistration = () => {
     return (
-        <div className="w-full text-center pt-32">
+        <div className="w-full text-center pt-48 pb-20">
             <h1 className="text-2xl font-bold mb-10">Registration for DL Enigma is yet to start!</h1>
             <a href="/dlenigma">
                 <PrimaryButton text="Go Back" />
