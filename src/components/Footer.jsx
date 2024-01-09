393609172f645b45686dd288c9ecd1fbbe724b2b
Mufassir-Chowdhury <mac22214u@gmail.com>
2024-01-09 16:22:40 +0600

    fixed footer alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,15 +10,15 @@ const Footer = () => {
         <footer className="bg-footer-texture bg-cover px-4 py-4 z-50 relative">
             <div className="flex justify-around items-center space-x-5 md:space-x-10 lg:space-x-32">
                 {/* First Section: Logo */}
-                <div className="lg:mr-8 ">
+                <div className="lg:mr-8 w-1/2 lg:w-1/3  ">
                     <img src="/image/logo/Carnival logo.png" alt="Footer Logo" className="h-32 lg:h-40" />
                 </div>
 
                 {/* Second Section: Links */}
-                <div className='flex flex-col sm:flex-row justify-between gap-6 sm:gap-20 md:gap-32'>
+                <div className='flex flex-col lg:flex-row w-1/2 lg:w-2/3 justify-around gap-6 lg:gap-20 '>
 
                     <div className="text-navbar-link text-sm">
-                        <p className="text-lg font-bold mb-4 hidden sm:block">Social Media Links</p>
+                        <p className="text-lg font-bold mb-4 hidden lg:block">Social Media Links</p>
                         <div className="flex space-x-4">
                             {/* Facebook */}
                             <a href="https://www.facebook.com/sustcsecarnival2024?mibextid=zLoPMf" target="_blank" rel="noopener noreferrer">
