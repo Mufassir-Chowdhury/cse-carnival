c7679eeb25d5d7f8ec24bc145a7b74b04012e1b2
Mufassir-Chowdhury <mac22214u@gmail.com>
2024-02-07 12:25:52 +0600

    added hackathon list

---
+++ src/data/hackathonSelectedTeams.js
@@ -0,0 +1,182 @@
+export const hackathonSelectedTeams = [
+    {
+      "name": "DU_Prometheus",
+      "university": "University of Dhaka",
+      "url": "https://csecarnival.sust.edu.bd/payment/14e3e657-926d-4c2b-8b84-43edeacf76f1"
+    },
+    {
+      "name": "\u0986\u09ac\u09b9\u09ae\u09be\u09a8",
+      "university": "University of Dhaka",
+      "url": "https://csecarnival.sust.edu.bd/payment/984ef2c6-c8d2-4318-b162-dcd2095e8bb3"
+    },
+    {
+      "name": "IUT Vanguard",
+      "university": "Islamic University of Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/5731a560-7901-4968-8851-34a7bcd3e60c"
+    },
+    {
+      "name": "BUET_Genesis",
+      "university": "Bangladesh University of Engineering & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/5ee8e641-d913-4738-9b66-70e1623c83d2"
+    },
+    {
+      "name": "Team_TechMinarets",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/e8017348-6473-4617-9651-097c0ef944f6"
+    },
+    {
+      "name": "RU Code Smiths",
+      "university": "University of Rajshahi",
+      "url": "https://csecarnival.sust.edu.bd/payment/d0c34514-6a03-41d3-ad5b-860ccbe900cc"
+    },
+    {
+      "name": "DU_E_BDY",
+      "university": "University of Dhaka",
+      "url": "https://csecarnival.sust.edu.bd/payment/78494b13-7aab-4fd1-b943-70008f288811"
+    },
+    {
+      "name": "BRUR_SAS",
+      "university": "Begum Rokeya University, Rangpur",
+      "url": "https://csecarnival.sust.edu.bd/payment/00ce2c3a-d626-4756-ab0f-5d70e46451ee"
+    },
+    {
+      "name": "Team_NoOoB",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/df57550b-fede-4360-9e5a-8109a98c91a4"
+    },
+    {
+      "name": "IUT GENESIS",
+      "university": "Islamic University of Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/ebf02a18-a25a-4549-bdbb-ee911a308556"
+    },
+    {
+      "name": "SUST Define Coders",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/fb700299-b039-4443-b0a8-762f0fd2a3b6"
+    },
+    {
+      "name": "UAP_Three_Musketeers",
+      "university": "University of Asia Pacific",
+      "url": "https://csecarnival.sust.edu.bd/payment/4aebcbe2-62e4-4f47-b742-16701a37e115"
+    },
+    {
+      "name": "Sust Tyro Trio",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/0c47aac6-f8cd-40bb-afb8-3bdbb7f7c808"
+    },
+    {
+      "name": "BUET_CodeComets",
+      "university": "Bangladesh University of Engineering & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/fc242223-b5d8-40df-91ad-4e06b9090b5b"
+    },
+    {
+      "name": "StackWhiz",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/6fc2d9e6-5a0d-4769-9ff0-ce1f307e16cb"
+    },
+    {
+      "name": "Bashmoti_Dim_190",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/7a08afd7-9454-46a5-9d5a-fcccac2dea62"
+    },
+    {
+      "name": "Shatab",
+      "university": "International University of Business Agriculture & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/8f4f03ed-df71-4c0d-a328-1b7655d58b88"
+    },
+    {
+      "name": "/dev/stderr",
+      "university": "University of Global Village",
+      "url": "https://csecarnival.sust.edu.bd/payment/5cd40007-e636-4ead-9c03-7ed2069a40af"
+    },
+    {
+      "name": "IUT Dev Mavericks",
+      "university": "Islamic University of Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/53ebf9de-bcc3-42c4-bab4-dcce662bcbde"
+    },
+    {
+      "name": "Team Return Zero",
+      "university": "Islamic University of Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/08e28652-27a3-4de0-b356-924d721e970e"
+    },
+    {
+      "name": "Team BitBusters",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/9b9d1d56-c5b9-4e32-9a65-caf874a044c2"
+    },
+    {
+      "name": "LU_Big_Brain",
+      "university": "Leading University",
+      "url": "https://csecarnival.sust.edu.bd/payment/f309b228-2946-49b6-aedf-ed977a38384f"
+    },
+    {
+      "name": "Developer_Manush",
+      "university": "Bangladesh Army International University of Science & Technology(BAIUST) ,Comilla",
+      "url": "https://csecarnival.sust.edu.bd/payment/0133ad5b-8e1e-4a76-b363-d47d53456094"
+    },
+    {
+      "name": "DU_Featherless_Bipeds",
+      "university": "University of Dhaka",
+      "url": "https://csecarnival.sust.edu.bd/payment/04244065-b0b1-4db4-926b-bdb667b9ac72"
+    },
+    {
+      "name": "Team ICT IU",
+      "university": "Islamic University, Bangladesh",
+      "url": "https://csecarnival.sust.edu.bd/payment/09c8c0be-f220-4a3d-8667-cae95365c689"
+    },
+    {
+      "name": "SUST_Kazei_PoriSoy",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/a495f07d-9d6c-41cf-9dc9-75117436faf8"
+    },
+    {
+      "name": "Heckermenz",
+      "university": "Bangladesh University of Engineering & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/122aea32-d724-4774-9a17-1144f8586349"
+    },
+    {
+      "name": "DU_sikhte_noy_jitte_eshechi",
+      "university": "University of Dhaka",
+      "url": "https://csecarnival.sust.edu.bd/payment/47e08047-5211-4d9e-b6fd-1ac5bdbca735"
+    },
+    {
+      "name": "Hack In Shell",
+      "university": "Islamic University of Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/7c1add4e-bda9-43b9-901b-febc0f72d654"
+    },
+    {
+      "name": "IUBAT",
+      "university": "International University of Business Agriculture & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/311ed822-60df-4b0b-864f-cfd92382dc34"
+    },
+    {
+      "name": "MIST_crackstation",
+      "university": "Military Institute of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/22686761-8a99-42dc-b737-aa13539fbeb7"
+    },
+    {
+      "name": "The Logic Legends",
+      "university": "Military Institute of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/47199264-d777-4f73-9206-33a65de4efe7"
+    },
+    {
+      "name": "BUET_Multithreading",
+      "university": "Bangladesh University of Engineering and Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/92935301-416c-4432-b85b-8b326621655b"
+    },
+    {
+      "name": "DevBots",
+      "university": "Shahjalal University of Science & Technology",
+      "url": "https://csecarnival.sust.edu.bd/payment/e6f76639-9763-4d98-b433-0da747a0aeaa"
+    },
+    {
+      "name": "DU_REKa",
+      "university": "University of Dhaka",
+      "url": "https://csecarnival.sust.edu.bd/payment/51c903ae-8a09-42e0-b065-752810dea8e7"
+    },
+    {
+      "name": "Team Recursion",
+      "university": "Chittagong Independent University",
+      "url": "https://csecarnival.sust.edu.bd/payment/8d99c4d0-199e-4629-82a6-c4187b77b5c0"
+    }
+  ]
--- src/pages/HackathonList.jsx
+++ src/pages/HackathonList.jsx
@@ -1,50 +1,22 @@
 import { SecondaryButton } from "../components/Button"
 import { useState } from "react";
 import { useEffect } from "react";
+import Marquee from "react-fast-marquee";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
+import { hackathonSelectedTeams } from "../data/hackathonSelectedTeams";
 
-const people = [
 
+const people = hackathonSelectedTeams;
 
-  {
-    name: 'Team Name',
-    university: 'University of Technology',
-    url: '/payment/id',
-  },
-  {
-    name: 'Team Name',
-    university: 'Fak University of Technology',
-    url: '/payment/id',
+// sort the people array by university name
+people.sort((a, b) => a.university.localeCompare(b.university));
 
-  },
-  {
-    name: 'Team Name',
-    university: 'Fa University of Technology',
-    url: '/payment/id',
-
-  },
-  {
-    name: 'Team Name',
-    university: 'F University of Technology',
-    url: '/payment/id',
-
-  },
-  {
-    name: 'Team Name',
-    university: 'Fakee University of Technology',
-    url: '/payment/id',
-
-  },
-  {
-    name: 'Team Name',
-    university: 'Fake University of Technology',
-    url: '/payment/id',
-
-  },
-]
 
 export default function HackathonList() {
   const [search, setSearch] = useState("");
   const [filteredPeople, setFilteredPeople] = useState(people);
+  {/*search by word given in input field*/ }
   const handleSearch = (e) => {
     const searchText = e.target.value.toLowerCase();
     setSearch(searchText);
@@ -59,32 +31,67 @@ export default function HackathonList() {
       )
     );
   }, [search]);
-
   return (
-    <ul className="divide-y divide-gray-100 p-6 pt-28 sm:p-28">
-      <h2 className="text-center text-3xl font-semibold mt-4">Hackathon Team List</h2>
-
-      <div className=" mt-4 flex justify-center ">
-        <input
-          type="text"
-          placeholder="Search by Team Name or University Name"
-          className="border-2 border-navbar rounded-md p-2 w-[80%] md:w-[50%]"
-          value={search}
-          onChange={handleSearch}
-        />
-
-      </div>
-
-      {filteredPeople.length === 0 && (
-        <div className="text-center text-2xl font-semibold mt-4 max-h-screen">No Team Found</div>
-      )}
+    <div className=" pt-24">
 
 
-      {filteredPeople.map((person, index) => (
+
+      <Marquee gradient={false} speed={100} className=" overflow-y-hidden bg-navbar-button-hover">
+        <p className="text-center text-md font-semibold py-1 text-white">**Hackathon Payment Deadline: 12th February, 2024** </p>
+      </Marquee>
+      <div className="container mx-auto min-h-screen">
+        <ul className="divide-y divide-gray-100 p-6 pt-0 sm:px-28 lg:px-48" >
+          {/* add a marquee to show the payment deadline repeated*/}
+          <h2 className="text-center text-3xl font-semibold mt-4">Hackathon Selected Teams</h2>
+          <div className=" my-4 flex justify-center ">
+            <input
+              type="text"
+              placeholder="Search by Team Name or University Name"
+              className="border-2 border-navbar rounded-md p-2 w-[80%] md:w-[50%]"
+              value={search}
+              onChange={handleSearch}
+
+            />
+
+          </div>
+          {/* {filteredPeople.length === 0 && <p className="text-center text-2xl mt-4 font-semibold max-h-screen">No Team Found</p>} */}
+
+          <li className="flex justify-between gap-x-2 md:gap-x-6 py-5 items-center bg-slate-900 bg-opacity-10 px-4 text-lg font-semibold">
+            <div className="flex min-w-0 gap-x-2 sm:gap-x-4 items-center">
+              <div className="min-w-0 flex-auto">
+                <p className="mt-1 truncate leading-5 ">Team name</p>
+              </div>
+            </div>
+            <div className="shrink-0 flex flex-col items-end">
+              <p className="leading-6 text-navbar">Payment Link</p>
+            </div>
+          </li>
+          {filteredPeople.length === 0 && <p className="text-center text-2xl mt-4 font-semibold max-h-screen">No Team Found</p>}
+          {filteredPeople.map((person, index) => (
+            <li key={person.url} className="flex justify-between odd:bg-slate-500 odd:bg-opacity-10 px-4 gap-x-2 md:gap-x-6 py-5 items-center">
+              <div className="grid grid-cols-12 min-w-0 gap-x-2 sm:gap-x-4 items-center">
+                <div className="h-6 w-6 col-span-1 md:h-12 md:w-12 flex rounded-full bg-gray-700 text-center text-white justify-center items-center">{index + 1}</div>
+                <div className="min-w-0 flex-auto col-span-9 pl-2">
+                  <p className="mt-1 truncate leading-5 ">{person.name}</p>
+                  <p className="mt-1 truncate text-sm leading-5 text-gray-500">{person.university}</p>
+                </div>
+              </div>
+              <div className="shrink-0 flex flex-col items-end">
+                <a href={person.url}>
+                  {/* use hyperlink icon */}
+                  <FontAwesomeIcon icon={faExternalLink} className="text-2xl text-navbar hover:text-navbar-button mr-10" />
+                  {/* <SecondaryButton text="Payment Link" /> */}
+                </a>
+
+              </div>
+            </li>
+          ))}
+
+          {/* {filteredPeople.map((person, index) => (
         <li key={person.url} className="flex justify-between gap-x-2 md:gap-x-6 py-5 items-center">
           <div className="flex min-w-0 gap-x-2 sm:gap-x-4 items-center">
             <div className="h-6 w-6 md:h-12 md:w-12 flex rounded-full bg-gray-700 text-center text-white justify-center items-center">{index + 1}</div>
-            {/* <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" /> */}
+            <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
             <div className="min-w-0 flex-auto">
               <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
               <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.university}</p>
@@ -96,7 +103,9 @@ export default function HackathonList() {
             </a>
           </div>
         </li>
-      ))}
-    </ul>
+      ))} */}
+        </ul>
+      </div>
+    </div>
   )
 }
